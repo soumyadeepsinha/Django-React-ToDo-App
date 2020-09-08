70c947bc019fb2223650f2fda241f38c5a5ed824
soumyadepsinha <s.sinha2701@gmail.com>
2020-09-08 18:50:26 +0530

    bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -20,15 +21,15 @@ class App extends React.Component {
     this.todoEdit = this.todoEdit.bind(this)
     this.todoDelete = this.todoDelete.bind(this)
     this.strikeUnstrike = this.strikeUnstrike.bind(this)
-  }
+  };
 
   getCookie(name) {
-    var cookieValue = null;
+    let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
-      var cookies = document.cookie.split(';');
-      for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].trim();
-        // Does this cookie string begin with the name we want?
+      let cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        // does this cookie string begin with the name we want?
         if (cookie.substring(0, name.length + 1) === (name + '=')) {
           cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
           break;
@@ -38,24 +39,28 @@ class App extends React.Component {
     return cookieValue;
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.fetchTask()
   }
 
   fetchTask() {
-    fetch('http://localhost:8000/api/task-list/')
-      .then((response) => response.json())
-      .then(data => {
+    console.log('Fetching...')
+
+    fetch('http://127.0.0.1:8000/api/task-list/')
+      .then(response => response.json())
+      .then(data =>
         this.setState({
-          todoList: data,
+          todoList: data
         })
-      })
+      )
   }
 
   handleChange(e) {
     let name = e.target.name
     let value = e.target.value
-    console.log('Name: ', name, 'Value: ', value);
+    console.log('Name: ', name)
+    console.log('Value: ', value)
+
     this.setState({
       activeItem: {
         // spread operator to update the child
@@ -67,7 +72,7 @@ class App extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault()
-    console.log('Item: ', this.state.activeItem);
+    console.log('Item: ', this.state.activeItem)
 
     let csrftoken = this.getCookie('csrftoken')
 
@@ -83,7 +88,7 @@ class App extends React.Component {
     fetch(url, {
       method: 'POST',
       headers: {
-        'content-type': 'application/json',
+        'Content-type': 'application/json',
         'X-CSRFToken': csrftoken,
       },
       body: JSON.stringify(this.state.activeItem)
@@ -94,20 +99,21 @@ class App extends React.Component {
           id: null,
           title: '',
           completed: false,
-        },
-        completed: false
+        }
       })
-    }).catch(function (err) {
-      console.log('Error: ', err);
+    }).catch(function (error) {
+      console.log('Erroe: ', error)
     })
+
   }
 
   todoEdit(task) {
     this.setState({
       activeItem: task,
-      editing: true
+      editing: true,
     })
   }
+
 
   todoDelete(task) {
     let csrftoken = this.getCookie('csrftoken')
@@ -119,15 +125,17 @@ class App extends React.Component {
         'X-CSRFToken': csrftoken,
       },
     }).then((response) => {
-      // reload todos after delete
+      // re-render tasks after delete
       this.fetchTask()
     })
   }
 
   strikeUnstrike(task) {
+
     task.completed = !task.completed
     let csrftoken = this.getCookie('csrftoken')
     let url = `http://127.0.0.1:8000/api/task-update/${task.id}/`
+
     fetch(url, {
       method: 'POST',
       headers: {
@@ -139,61 +147,56 @@ class App extends React.Component {
       this.fetchTask()
     })
 
-    console.log('Task', task.completed);
+    console.log('Task :', task.completed)
   }
 
   render() {
-    let task = this.state.todoList
+    let tasks = this.state.todoList
     let self = this
     return (
-      <React.Fragment>
-        <div className="container">
-          <div id="task-container">
-            <div id="form-wrapper">
-              <form onSubmit={this.handleSubmit} id="form">
-                <div className="flex-wrapper">
+      <div className="container">
+        <div id="task-container">
+          <div id="form-wrapper">
+            <form onSubmit={this.handleSubmit} id="form">
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input onChange={this.handleChange} className="form-control" id="title" value={this.state.activeItem.title} type="text" name="title" placeholder="Add your news tasks..." />
+                </div>
 
-                  <div style={{ flex: 6 }}>
-                    <input onChange={this.handleChange} id="title" className="form-control" name="title" value={this.state.activeItem.title} type="text" autoComplete="off" placeholder="Whats you next task" />
+                <div style={{ flex: 1 }}>
+                  <input id="submit" className="btn btn-warning" type="submit" value="Add a new task" />
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div id="list-wrapper">
+            {tasks.map(function (task, index) {
+              return (
+                <div key={index} className="task-wrapper flex-wrapper">
+                  <div onClick={() => self.strikeUnstrike(task)} style={{ flex: 7 }}>
+
+                    {task.completed === false ?
+                      (<span>{task.title}</span>) :
+                      (<strike>{task.title}</strike>)
+                    }
                   </div>
 
                   <div style={{ flex: 1 }}>
-                    <input id="submit" className="btn btn-warning" type="submit" value="Add a new task" />
+                    <button onClick={() => self.todoEdit(task)} className="btn btn-sm btn-outline-info">Edit</button>
                   </div>
 
+                  <div style={{ flex: 1 }}>
+                    <button onClick={() => self.todoDelete(task)} className="btn btn-sm btn-outline-danger delete">x</button>
+                  </div>
                 </div>
-              </form>
-            </div>
-            <div id="list-wrapper">
-              {task.map(function (task, index) {
-                return (
-                  <div onClick={() => self.strikeUnstrike(task)} key={index} className="task-wrapper flex-wrapper">
-
-                    <div style={{ flex: 7 }}>
-                      {task.completed === false ?
-                        (<span>{task.title}</span>) :
-                        (<strike>{task.title}</strike>)
-                      }
-                    </div>
-
-                    <div style={{ flex: 1 }}>
-                      <button onClick={() => self.todoEdit(task)} className="btn btn-sm btn-outline-info">Edit</button>
-                    </div>
-
-                    <div style={{ flex: 2 }}>
-                      <button onClick={() => self.todoDelete(task)} className="btn btn-sm btn-outline-dark delete">x</button>
-                    </div>
-
-                  </div>
-                )
-              })}
-            </div>
+              )
+            })}
           </div>
         </div>
-      </React.Fragment>
-    );
+      </div>
+    )
   }
-
 }
 
 export default App;
