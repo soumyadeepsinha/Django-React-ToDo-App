b0951ee08cbba456b3903dc15db63cd18b313796
soumyadepsinha <s.sinha2701@gmail.com>
2020-09-08 10:39:17 +0530

    add todo

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ class App extends React.Component {
       editing: false,
     }
     this.fetchTask = this.fetchTask.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentDidMount() {
@@ -31,6 +33,44 @@ class App extends React.Component {
       })
   }
 
+  handleChange(e) {
+    let name = e.target.name
+    let value = e.target.value
+    console.log('Name: ', name, 'Value: ', value);
+    this.setState({
+      activeItem: {
+        // spread operator to update the child
+        ...this.state.activeItem,
+        title: value
+      }
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log('Item: ', this.state.activeItem);
+    let url = ' http://127.0.0.1:8000/api/task-create/'
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(this.state.activeItem)
+    }).then((response) => {
+      this.fetchTask()
+      this.setState({
+        activeItem: {
+          id: null,
+          title: '',
+          completed: false,
+        },
+        completed: false
+      })
+    }).catch(function (err) {
+      console.log('Error: ', err);
+    })
+  }
+
   render() {
     let task = this.state.todoList
     return (
@@ -38,10 +78,10 @@ class App extends React.Component {
         <div className="container">
           <div id="task-container">
             <div id="form-wrapper">
-              <form id="form">
+              <form onSubmit={this.handleSubmit} id="form">
                 <div className="flex-wrapper">
                   <div style={{ flex: 6 }}>
-                    <input id="title" className="form-control" name="title" type="text" placeholder="Whats you next task" />
+                    <input onChange={this.handleChange} id="title" className="form-control" name="title" type="text" autoComplete="off" placeholder="Whats you next task" />
                   </div>
                   <div style={{ flex: 1 }}>
                     <input id="submit" className="btn btn-warning" type="submit" name="Add" value="Add a new task" />
@@ -53,7 +93,15 @@ class App extends React.Component {
               {task.map(function (task, index) {
                 return (
                   <div key={index} className="task-wrapper flex-wrapper">
-                    <span>{task.title}</span>
+                    <div style={{ flex: 7 }}>
+                      <span>{task.title}</span>
+                    </div>
+                    <div style={{ flex: 1 }}>
+                      <button className="btn btn-sm btn-outline-info">Edit</button>
+                    </div>
+                    <div style={{ flex: 2 }}>
+                      <button className="btn btn-sm btn-outline-dark delete">x</button>
+                    </div>
                   </div>
                 )
               })}
